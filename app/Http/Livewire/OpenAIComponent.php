6b5660a4b3e18c9b1bdc947e93af612bc54c8918
Satrio Adi <satrio.adi1021@gmail.com>
2023-06-24 16:12:08 +0700

    [IMPROVE] Add lines of codes

--- app/Http/Livewire/OpenAIComponent.php
+++ app/Http/Livewire/OpenAIComponent.php
@@ -17,16 +17,24 @@ class OpenAIComponent extends Component
 
     public function generateCompletion()
     {
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . config('services.openai.api_key'),
-            'Content-Type' => 'application/json',
-        ])->post('https://api.openai.com/v1/engines/davinci/completions', [
-            'prompt' => $this->prompt,
-            'max_tokens' => 100,
+        $this->validate([
+            'prompt' => 'required',
         ]);
 
-        $completion = json_decode($response->getBody(), true);
-        dd($completion);
-        $this->output = $completion['choices'][0]['text'];
+        try {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . config('services.openai.api_key'),
+                'Content-Type' => 'application/json',
+            ])->post('https://api.openai.com/v1/engines/davinci/completions', [
+                'prompt' => $this->prompt,
+                'max_tokens' => 100,
+            ]);
+
+            $completion = json_decode($response->getBody(), true);
+            $this->output = $completion['choices'][0]['text'];
+        } catch (\Exception $e) {
+            // Menangani error
+            $this->output = 'Error: ' . $e->getMessage();
+        }
     }
 }
